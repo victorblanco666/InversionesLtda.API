831d9299c0c789b780266adf88cceaf83ed0d3c8
VB666 <vict.gamboa@duocuc.cl>
2024-06-29 16:32:22 -0400

    Creacion de Modelo Producto

---
+++ API C#/InversionesLtda.API/Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace InversionesLtda.API.Models
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string producto { get; set; }
+        public string precio { get; set; }
+        public string stock { get; set; }
+    }
+}
